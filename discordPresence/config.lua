e89550706a16e529b8f9c8e81e693f80157b8b6f
faisalashraf1958 <79255645+faisalashraf1958@users.noreply.github.com>
2021-04-05 09:54:58 +0530

    Add files via upload

---
+++ discordPresence/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+Config.ClientID = 
+Config.PlayerCount = 32
+Config.PlayerText = "Players" -- Player text. Example Players 10/32
+Config.ResourceTimer = 5 -- Time in witch resource updates in seconds
+Config.UseESXIdentity = false -- Uses ESX Identity name not steam name
+Config.Buttons = { -- Your buttons
+    {index = 0,name = '',url = ''},
+    {index = 1,name = '',url = ''}
+}
